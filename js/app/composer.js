52d458a96e5fc308daf5005dc83613ddd2c8c129
Felix Palmer <felixpalmer@gmail.com>
2014-03-17 14:28:36 +0100

    Add edge pass filter

--- js/app/composer.js
+++ js/app/composer.js
@@ -1,9 +1,18 @@
-define( ["three", "camera", "renderer", "scene"], function ( THREE, camera, renderer, scene ) {
+define( ["three", "camera", "container", "renderer", "scene"], function ( THREE, camera, container, renderer, scene ) {
   var composer = new THREE.EffectComposer( renderer );
 
+  // Render actual scene
   var renderPass = new THREE.RenderPass( scene, camera );
   composer.addPass( renderPass );
 
+  // Start postprocessing
+  // Edge detection
+  var edgePass = new THREE.ShaderPass( THREE.EdgeShader );
+  edgePass.uniforms.aspect.value.x = container.offsetWidth;
+  edgePass.uniforms.aspect.value.y = container.offsetWidth;
+  composer.addPass( edgePass );
+
+  // Copy final result to screen
   var copyPass = new THREE.ShaderPass( THREE.CopyShader );
   copyPass.renderToScreen = true;
   composer.addPass( copyPass );
